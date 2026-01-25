cc1f7b535bbf576886c781f4b10348556e847a73
dhruvawani17 <dhruvawani17@gmail.com>
2026-01-25 23:21:21 +0530

    fasten up website

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// Font imports
+import "@fontsource/inter";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/bangers";
+import "@fontsource/fredoka";
+import "@fontsource/fredoka/300.css";
+import "@fontsource/fredoka/500.css";
+import "@fontsource/fredoka/600.css";
+import "@fontsource/fredoka/700.css";
+import "@fontsource/playfair-display";
+import "@fontsource/playfair-display/400-italic.css";
+import "@fontsource/playfair-display/500.css";
+import "@fontsource/playfair-display/600.css";
+import "@fontsource/playfair-display/700.css";
+import "@fontsource/playfair-display/800.css";
+import "@fontsource/playfair-display/900.css";
+import "@fontsource/sacramento";
+import "@fontsource/sora";
+import "@fontsource/sora/100.css";
+import "@fontsource/sora/200.css";
+import "@fontsource/sora/300.css";
+import "@fontsource/sora/500.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
+import "@fontsource/sora/800.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
