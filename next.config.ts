64dd1ab93b92d2ed95d651df61f4a73dad0b24e5
dhruvawani17 <dhruvawani17@gmail.com>
2026-09-14 00:33:56 +0530

    Redirect /mac to https://mac-ochre.vercel.app

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: "/mac",
         destination: "https://mac-ochre.vercel.app",
+        permanent: true,
+      },
+      {
+        source: "/mac/:path*",
+        destination: "https://mac-ochre.vercel.app/:path*",
+        permanent: true,
       },
     ];
   },
